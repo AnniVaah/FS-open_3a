a7f3d071093d6082b834cce8c7a9ac93f55d785b
Anni <anni.o.vaahtoranta@gmail.com>
2024-04-10 15:08:17 +0300

    3.13

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
+const Person = require('./models.person')
 
-/* const requestLogger = (request, response, next) => {
+const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
     console.log('Body:  ', request.body)
     console.log('---')
     next()
-} */
+}
 
 app.use(express.json())
-//app.use(requestLogger)
+app.use(requestLogger)
 app.use(morgan('tiny'))
 app.use(cors())
 app.use(express.static('dist'))
 
-let persons = [
+ let persons = [/*
     {
       id: 1,
       name: "Arto Hellas",
@@ -39,14 +42,17 @@ let persons = [
         number: "39-23-6423122"      
     },
 
-]
+*/] 
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
+    
 })
 
 app.get('/info', (request, response) => {
@@ -121,7 +127,7 @@ const unknownEndpoint = (request, response) => {
   
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
